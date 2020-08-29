123dde1b3bb11832053f04510e55f4cb3d067bc4
ismailnacer <43656363+ismailnacer@users.noreply.github.com>
2020-08-30 00:37:33 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN: "NzQ5MjcwOTkyNDIwODY0MDQw.X0pi_Q.ZpF4KXKQgcYW4CuGL6whmvNTDzE", //تعديل مهم توكن بوتك
+  YT_API_KEY: "api ",
+  prefix: "=",
+  devs: ["709875049498148864"] ///تعديل مهم غير هذا الرقم بـ ايدي حسابك
+};
